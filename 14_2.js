baf12f9f097b620c0d9a906ade5ec6b55ee1b820
Jonathan Wilson <jonathansw@comcast.net>
2023-05-04 18:45:52 -0400

    first commit

---
+++ 14_2.js
@@ -0,0 +1,32 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+//the 2015 way to sto9p re-assignment is
+
+const pi = 3.14;
+
+// What is the difference between var and let?
+
+//var is old, let is post 2015
+//var you can redeclair, let you cannot.  var is function scope, let is block scope
+
+
+
+
+
+
+//What is the difference between var and const?
+//const is post 2015
+//const you cannot reassign, or redeclair.  const is block scope
+
+
+//What is the difference between let and const?
+//  you cannot reassign const like you can let
+
+
+
+//What is hoisting? - the declarations are run before the rest of the code;  let and cost do not have hoisting, 
+//the declarations are run in the order from everything else.  
+//Also cannot declare a variable with const and have it be undefined like you can with var;
+
